827140f0b9523bdb98bd0d03408c2df11b9c60cb
Lucas Belluzi <lucasdejesusoliveirabelluzi@gmail.com>
2026-01-28 19:16:21 -0300

    feat(app): creating main components - adding the css style - creating the HTML structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing-module';
-import { App } from './app';
+import { AppRoutingModule } from './app-routing.module';
+import { App } from './app.component';
+import { QuizzComponent } from './components/quizz/quizz.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    QuizzComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
